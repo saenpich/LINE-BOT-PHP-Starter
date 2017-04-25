1c21bb3397dce4be6e186c7d2a94e6716186c6e9
saenpich <mac.147@hotmail.com>
2017-04-25 11:14:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,3 +18,10 @@ echo "Hello LINE BOT";
       }
   ]
 }
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
